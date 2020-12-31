5114e1cd965304bb4dc5c3dd981c881755a79b91
softkaikai <282748212@qq.com>
2020-12-31 16:41:52 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+ <div>
+  <div class="red">sdfsdf</div>
+  <div class="red">{{name}}</div>
+ </div>
+</template>
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'AppComponent',
+        data() {
+            return {
+                name: 'kaikai'
+            }
+        },
+        mounted() {
+            console.log(1111);
+        }
+    })
+</script>
+
+<style scoped>
+    .red {
+        color: red;
+    }
+</style>
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue';
+import AppComponent from './App.vue';
+
+const index = createApp(AppComponent);
+const vm = index.mount('#app')
